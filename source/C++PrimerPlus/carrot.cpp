0c71fc08523c744222f8ae2c4c93bdab50c8e013
LiuXuan2096 <liuxuan2096@icloud.com>
2022-10-08 19:41:58 +0800

    page 21

---
+++ source/C++PrimerPlus/carrot.cpp
@@ -0,0 +1,21 @@
+/*
+page 21
+*/
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int carrots;
+
+    carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots.";
+    cout << endl;
+    carrots = carrots - 1;
+    cout << "Crunch, crunch. Now I hava " << carrots << " carrots." << endl;
+    return 0;
+}
