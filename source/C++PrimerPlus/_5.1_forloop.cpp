1084b08e5df3f54e98e5880efc0b42fb3c04c978
LiuXuan2096 <liuxuan2096@icloud.com>
2022-10-15 18:39:53 +0800

    page125 forloop

---
+++ source/C++PrimerPlus/_5.1_forloop.cpp
@@ -0,0 +1,16 @@
+// page 125 forloop
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        cout << "C++ knows loops.\n";
+    }
+    cout << "C++ knows when to stop.\n";
+    return 0;
+    
+}
