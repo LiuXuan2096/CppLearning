00cc652ee44f0f520306baa0d9cea0776d2dfdf0
LiuXuan <liuxuan2096@icloud.com>
2022-11-07 13:51:58 +0800

    指针运算

---
+++ source/C_C++基础/_6.5_指针运算.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float* p = (float*)calloc(10, sizeof(float));
+    if (p == NULL)
+    {
+        /* code */
+        exit(1);
+    }
+
+    printf("p = %p, p+1 = %p, p+3 = %p\n", p, p+1, p+3);
+    for (unsigned int i = 0; i < 10; i++)
+    {
+        /* code */
+        *(p++) = i;
+    }
+
+    p -= 10;
+    printf("p[0] = %f, p[3] = %f, p[9] = %f\n", *p, *(p+3), *(p+9));
+    
+    return 0;
+}
