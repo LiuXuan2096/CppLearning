c53ac17c1d97378b8e4caac1fa8d497eaab62836
LiuXuan <liuxuan2096@icloud.com>
2022-11-07 21:49:51 +0800

    第二个课堂代码

--- source/C_C++基础/_6.5_指针运算.cpp
+++ source/C_C++基础/_6.5_指针运算.cpp
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
-    float* p = (float*)calloc(10, sizeof(float));
-    if (p == NULL)
+    // float* p = (float*)calloc(10, sizeof(float));
+    // if (p == NULL)
+    // {
+    //     /* code */
+    //     exit(1);
+    // }
+
+    // printf("p = %p, p+1 = %p, p+3 = %p\n", p, p+1, p+3);
+    // for (unsigned int i = 0; i < 10; i++)
+    // {
+    //     /* code */
+    //     *(p++) = i;
+    // }
+
+    // p -= 10;
+    // printf("p[0] = %f, p[3] = %f, p[9] = %f\n", *p, *(p+3), *(p+9));
+    // free(p);
+
+    char s[] = "Mary has 3 lambs.";
+    char* p = s;
+
+    while (*p != 0)
     {
         /* code */
-        exit(1);
+        if (islower(*p))
+        {
+            /* code */
+            *p = *p + ('A' - 'a');
+        }
+        p++;
     }
-
-    printf("p = %p, p+1 = %p, p+3 = %p\n", p, p+1, p+3);
-    for (unsigned int i = 0; i < 10; i++)
-    {
-        /* code */
-        *(p++) = i;
-    }
-
-    p -= 10;
-    printf("p[0] = %f, p[3] = %f, p[9] = %f\n", *p, *(p+3), *(p+9));
-    free(p);
     
+    printf("s = %s", s);
     return 0;
 }
