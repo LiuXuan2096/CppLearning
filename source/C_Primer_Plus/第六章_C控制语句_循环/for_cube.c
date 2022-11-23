cecc12d610211cafac3a384b06a1add5291fc2dd
LiuXuan <liuxuan2096@icloud.com>
2022-11-23 23:54:49 +0800

    C Primer Plus第六章C控制语句:循环

---
+++ source/C_Primer_Plus/第六章_C控制语句_循环/for_cube.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("    n   n cubed\n");
+    for (num = 1; num <= 6; num++)
+    {
+        /* code */
+        printf("%5d %5d\n", num, num * num * num);
+    }
+
+    return 0;
+    
+}
---
+++ source/C_Primer_Plus/第六章_C控制语句_循环/sweetie2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const int NUMBER = 22;
+    int count;
+
+    for (count = 1; count <= NUMBER; count++)
+    {
+        /* code */
+        printf("Be my Valentine!\n");
+    }
+
+    return 0;
+}
